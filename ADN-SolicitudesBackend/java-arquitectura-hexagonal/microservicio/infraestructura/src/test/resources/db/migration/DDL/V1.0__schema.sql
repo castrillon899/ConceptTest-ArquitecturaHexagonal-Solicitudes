fed21f887361e4a967d734218eb1f5188563b4e4
Angel Ernesto Marin Castrillon - Ceiba Software <angel.marin@ceiba.com.co>
2021-07-26 09:23:43 -0500

    jenkins configuracion

---
+++ ADN-SolicitudesBackend/java-arquitectura-hexagonal/microservicio/infraestructura/src/test/resources/db/migration/DDL/V1.0__schema.sql
@@ -0,0 +1,44 @@
+create table usuario (
+ id int(11) not null auto_increment,
+ nombre varchar(100) not null,
+ clave varchar(45) not null,
+ fecha_creacion datetime null,
+ primary key (id)
+);
+
+
+
+
+create table solicitud (
+ id int(11) not null auto_increment,
+ descripcion varchar(100) not null,
+ tipo_de_solicitud varchar(100) not null,
+ estado varchar(100) not null,
+ fecha_creacion datetime null,
+ fecha_actualizacion datetime null,
+ fecha_tentativa_respuesta datetime null,
+ fecha_maxima_respuesta datetime null,
+ id_usuario_asignado int(11),
+ respuesta_solicitud varchar(4000),
+ id_cliente int(11),
+ cliente_celular_contacto varchar(50),
+ primary key (id)
+);
+
+
+create table role (
+ id int(11) not null auto_increment,
+ nombre varchar(100) not null,
+ primary key (id)
+);
+
+
+create table role_usuario (
+ id int(11) not null auto_increment,
+ id_usuario int(11) not null,
+ id_role int(11) not null,
+ primary key (id)
+);
+
+
+
